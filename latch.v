7046ebad435895092ae1a682a3559f2f7d0f87be
Hayden Miller <149623251+General-Ginger@users.noreply.github.com>
2024-12-13 01:15:04 -0800

    MEMORY

--- latch.v
+++ latch.v
@@ -1,68 +1,45 @@
-`include "adder stuff/adder.v"
-`include "ALU CONTROL stuff/alu_control.v"
-`include "ALU stuff/alu.v"
-`include "MUX stuff/mux.v"
-`include "MUX 2 stuff/mux_5bit.v"
+`include "branch stuff/branch.v"
+`include "memory stuff/memory.v"
 
 module latch (
-    input [1:0] wb_ctlout_in,
-    input [2:0] m_ctlout_in,
-    input [3:0] ex_ctlout,
-    input [31:0] npcout,
-    input [31:0] rdata1out,
-    input [31:0] rdata2out_in,
-    input [31:0] s_extendout,
-    input [4:0] instrout_2016,
-    input [4:0] instrout_1511,
-    output [1:0] wb_ctlout,
-    output [2:0] m_ctlout,
-    output [31:0] add_result,
-    output zero,
-    output [31:0] alu_result,
-    output [31:0] rdata2out,
-    output [4:0] five_bit_muxout
+    input clk,
+    input reset,
+    input Branch,
+    input Zero,
+    input MemRead,
+    input MemWrite,
+    input [31:0] alu_result_in,
+    input [31:0] write_data,
+    input [4:0] write_reg,
+    input [1:0] control_wb_in,
+    output PCSrc,
+    output [31:0] read_data,
+    output [31:0] mem_ALU_result,
+    output [4:0] mem_Write_reg,
+    output [1:0] mem_control_wb
 );
-    
-    wire [31:0] mux1_out;
-    wire [3:0] select_wire;
-    wire [1:0] variable = wb_ctlout_in;
-    assign wb_ctlout = variable;
-    wire [1:0] variable2 = m_ctlout_in;
-    assign m_ctlout = variable2;
-    
 
-    adder adder(
-        .A(npcout),
-        .B({26'b0, s_extendout[5:0]}),
-        .Y(add_result) 
+    wire variable = control_wb_in;
+    assign mem_control_wb = variable;
+    wire variable2 = alu_result_in;
+    assign mem_ALU_result = variable2;
+    wire variable3 = write_reg;
+    assign mem_Write_reg = variable3;
+    wire branch_decision;
+
+    branch branch (
+        .m_ctlout(Branch),
+        .zero(Zero),
+        .PCSrc(PCSrc)
     );
 
-    mux mux1(
-        .A({26'b0, s_extendout[5:0]}),
-        .B(rdata2out_in),
-        .sel(ex_ctlout[3]),
-        .Y(mux1_out)
-    );
-
-    mux_5bit mux2(
-        .A(instrout_1511),
-        .B(instrout_2016),
-        .sel(ex_ctlout[0]),
-        .Y(five_bit_muxout)
-    );
-
-    alu_control alu_control(
-        .alu_op(ex_ctlout[2:1]),
-        .funct(s_extendout[5:0]),
-        .select(select_wire)
-    );
-
-    alu alu(
-        .A(rdata1out),
-        .B(mux1_out),
-        .control(select_wire),
-        .Y(alu_result),
-        .zero(zero)
+    memory memory (
+        .clk(clk),
+        .MemRead(MemRead),
+        .MemWrite(MemWrite),
+        .address(alu_result_in),
+        .write_data(write_data),
+        .read_data(read_data)
     );
 
 endmodule
--- latch_tb.v
+++ latch_tb.v
@@ -3,41 +3,38 @@
 
 module latch_tb;
 
-    reg [1:0] wb_ctlout_in;
-    reg [2:0] m_ctlout_in;
-    reg [3:0] ex_ctlout;
-    reg [31:0] npcout;
-    reg [31:0] rdata1out;
-    reg [31:0] rdata2out_in;
-    reg [31:0] s_extendout;
-    reg [4:0] instrout_2016;
-    reg [4:0] instrout_1511;
-
-    wire [1:0] wb_ctlout;
-    wire [2:0] m_ctlout;
-    wire [31:0] add_result;
-    wire zero;
-    wire [31:0] alu_result;
-    wire [31:0] rdata2out;
-    wire [4:0] five_bit_muxout;
+    reg clk;
+    reg reset;
+    reg Branch;
+    reg Zero;
+    reg MemRead;
+    reg MemWrite;
+    reg [31:0] alu_result_in;
+    reg [31:0] write_data;
+    reg [4:0] write_reg;
+    reg [1:0] control_wb_in;
+    wire PCSrc;
+    wire [31:0] read_data;
+    wire [31:0] mem_ALU_result;
+    wire [4:0] mem_Write_reg;
+    wire [1:0] mem_control_wb;
 
     latch uut (
-        .wb_ctlout_in(wb_ctlout_in),
-        .m_ctlout_in(m_ctlout_in),
-        .ex_ctlout(ex_ctlout),
-        .npcout(npcout),
-        .rdata1out(rdata1out),
-        .rdata2out_in(rdata2out_in),
-        .s_extendout(s_extendout),
-        .instrout_2016(instrout_2016),
-        .instrout_1511(instrout_1511),
-        .wb_ctlout(wb_ctlout),
-        .m_ctlout(m_ctlout),
-        .add_result(add_result),
-        .zero(zero),
-        .alu_result(alu_result),
-        .rdata2out(rdata2out),
-        .five_bit_muxout(five_bit_muxout)
+        .clk(clk),
+        .reset(reset),
+        .Branch(Branch),
+        .Zero(Zero),
+        .MemRead(MemRead),
+        .MemWrite(MemWrite),
+        .alu_result_in(alu_result_in),
+        .write_data(write_data),
+        .write_reg(write_reg),
+        .control_wb_in(control_wb_in),
+        .PCSrc(PCSrc),
+        .read_data(read_data),
+        .mem_ALU_result(mem_ALU_result),
+        .mem_Write_reg(mem_Write_reg),
+        .mem_control_wb(mem_control_wb)
     );
 
     initial begin
@@ -45,38 +42,21 @@ module latch_tb;
         $dumpfile("latch_tb.vcd");
         $dumpvars(0, latch_tb);
 
-        wb_ctlout_in = 2'b01;
-        m_ctlout_in = 3'b101;
-        ex_ctlout = 4'b1100;
-        npcout = 32'h00000010;
-        rdata1out = 32'h00000005;
-        rdata2out_in = 32'h0000000A;
-        s_extendout = 32'h00000003;
-        instrout_2016 = 5'b00010;
-        instrout_1511 = 5'b00101;
-        #10;
+        clk = 0;
+        reset = 1;
+        Branch = 0;
+        Zero = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        alu_result_in = 0;
+        write_data = 0;
+        write_reg = 0;
+        control_wb_in = 0;
 
-        wb_ctlout_in = 2'b10;
-        m_ctlout_in = 3'b011;
-        ex_ctlout = 4'b1010;
-        npcout = 32'h00000020;
-        rdata1out = 32'h00000015;
-        rdata2out_in = 32'h0000001A;
-        s_extendout = 32'h00000006;
-        instrout_2016 = 5'b01010;
-        instrout_1511 = 5'b01111;
-        #10;
-
-        wb_ctlout_in = 2'b11;
-        m_ctlout_in = 3'b111;
-        ex_ctlout = 4'b1111;
-        npcout = 32'h00000030;
-        rdata1out = 32'h00000025;
-        rdata2out_in = 32'h0000002A;
-        s_extendout = 32'h00000009;
-        instrout_2016 = 5'b10010;
-        instrout_1511 = 5'b10101;
-        #10;
+        #10 reset = 0; Branch = 1; Zero = 1; alu_result_in = 32'h00000010; MemRead = 1; #10;
+        MemRead = 0; MemWrite = 1; write_data = 32'hDEADBEEF; alu_result_in = 32'h00000020; #10;
+        MemWrite = 0; MemRead = 1; alu_result_in = 32'h00000020; #10;
+        Branch = 0; Zero = 0; control_wb_in = 2'b10; write_reg = 5'b10101; #10;
 
         $finish;
     end
